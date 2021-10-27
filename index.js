ee2ec582fe5185c7ed5507481a8e37c9bd095e7a
learnerVarsha <89792897+learnerVarsha@users.noreply.github.com>
2021-10-27 19:06:27 +0530

    adding refreshMe function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var randomNumber1 = (Math.floor(Math.random()*6))+1;
-var randomNumber2 = (Math.floor(Math.random()*6))+1;
+function refreshMe(){
+  document.querySelector("#p1").textContent = "Player 1";
+  document.querySelector("#p2").textContent = "Player 2";
+  var randomNumber1 = (Math.floor(Math.random()*6))+1;
+  var randomNumber2 = (Math.floor(Math.random()*6))+1;
 
-document.querySelector("#diceForPlayer1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector("#diceForPlayer2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  document.querySelector("#diceForPlayer1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector("#diceForPlayer2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if(randomNumber1>randomNumber2){
   document.querySelector("h1").textContent = "Player 1 Wins!";
@@ -15,3 +18,5 @@ else if(randomNumber1<randomNumber2){
 else{
   document.querySelector("h1").textContent = "Draw!";
 }
+}
+
